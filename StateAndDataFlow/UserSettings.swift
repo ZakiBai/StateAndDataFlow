bf2388b3620974b2343f4942fbc3a505e95ac77f
Zaki <menza.zm@gmail.com>
2023-06-19 13:53:51 +0600

    User Settings done

---
+++ StateAndDataFlow/UserSettings.swift
@@ -0,0 +1,13 @@
+//
+//  UserSettings.swift
+//  StateAndDataFlow
+//
+//  Created by Zaki on 19.06.2023.
+//
+
+import Foundation
+
+final class UserSettings: ObservableObject {
+    @Published var user = false
+    var name = ""
+}
