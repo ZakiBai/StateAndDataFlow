56828732f77db6d07f8434dbe3aea18233db5d76
Zaki <menza.zm@gmail.com>
2023-06-19 14:01:32 +0600

    Root View is done

---
+++ StateAndDataFlow/RootView.swift
@@ -0,0 +1,29 @@
+//
+//  RootView.swift
+//  StateAndDataFlow
+//
+//  Created by Zaki on 19.06.2023.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    @StateObject private var user = UserSettings()
+    
+    var body: some View {
+        Group {
+            if user.isLoggedIn {
+                ContentView()
+            } else {
+                LoginView()
+            }
+        }
+        .environmentObject(user)
+    }
+}
+
+struct RootView_Previews: PreviewProvider {
+    static var previews: some View {
+        RootView()
+    }
+}
