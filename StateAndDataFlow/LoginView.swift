a4f28dd00376915703d93bf1ee839e185f671ed0
Zaki <menza.zm@gmail.com>
2023-06-19 14:03:30 +0600

    Login View is done

---
+++ StateAndDataFlow/LoginView.swift
@@ -0,0 +1,41 @@
+//
+//  LoginView.swift
+//  StateAndDataFlow
+//
+//  Created by Zaki on 19.06.2023.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @State private var name = ""
+    @EnvironmentObject private var user: UserSettings
+    
+    var body: some View {
+        
+        VStack(spacing: 20) {
+            TextField("Type your name", text: $name)
+                .multilineTextAlignment(.center)
+            
+            Button(action: {}) {
+                HStack {
+                    Image(systemName: "checkmark.circle")
+                    Text("OK")
+                }
+            }
+        }
+    }
+    private func login() {
+        if !name.isEmpty {
+            user.name = name
+            user.isLoggedIn.toggle()
+        }
+}
+
+
+}
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
